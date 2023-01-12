e4f7b8fb328e338664bb1458032932d305ef0264
Jose-Victor <josevitorppessoa@gmail.com>
2023-01-12 10:02:43 -0300

    alterando filtro da rota colaborador para UUID

--- src/controllers/colaborador/ReadColaboradorController.ts
+++ src/controllers/colaborador/ReadColaboradorController.ts
@@ -21,11 +21,11 @@ export class ReadColaboradorController {
 
 
   async colaborador(req: Request, res: Response) {
-    const {cpf} = req.params
+    const {uuid} = req.params
     try {
       const colaborador = await prismaClient.colaborador.findUnique({
         where: {
-          cpf
+          uuid
         }
       })
     
